4ed37ff8d345c233a1dae0c7b861c86a68b39f77
Jules FEDIT <jules.fedit@edu.ece.fr>
2023-05-17 00:18:12 +0200

    page des competences etudiant

---
+++ res/page_mes_competences.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="page_mes_competences.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" type="icon_onglet.png" href="icon_onglet.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Mes compétences</title>
+</head>
+
+<body>
+    <div id="header">
+        <table>
+            <tr>
+                <img class="logo" src="logo_skills_tracker_noir.png" alt="logo">
+            </tr>
+            <tr>
+                <button class="b1"> <strong>Mon compte</strong></button>
+            </tr>
+            <tr>
+                <button class="b1"> <strong>Mes compétences</strong></button>
+            </tr>
+            <tr>
+                <button class="b1"> <strong>A propos</strong></button>
+            </tr>
+            <tr>
+                <button class="b1"> <strong>Contact</strong></button>
+            </tr>
+        </table>
+    </div>
+    <div id="wrapper">
+        <br>
+        <h1> <strong> <span id="titrePage">MES COMPETENCES</span> </strong></h1>
+        <div id="emplacement_filtres">
+            <button class="filtres"><img src="filter.svg" alt="icon"> FILTRES</button>
+        </div>
+        <div id="navigation">
+            <strong>
+                <div id="titreOnglet">
+                    <span class="texteOnglet1">Mes compétences</span>
+                    <a href="page_competences_transverses.php" class="texteOnglet2">Mes compétences transverses</a>
+                    <a href="page_toutes_competences.php" class="texteOnglet3">Toutes mes compétences</a>
+                </div>
+            </strong>
+            <br>
+            <div class="matiere">
+                <div class="matieres">
+                    <table>
+                        <tr class="ligne">
+                            <span class="titreMatieres">MATHEMATIQUES :</span>
+                            <ul>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                            </ul>
+                        </tr>
+                        <tr>
+                            <span class="titreMatieres"> PHYSIQUE-CHIMIE :</span>
+                            <ul>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                            </ul>
+                        </tr>
+                        <tr class="ligne">
+                            <span class="titreMatieres">ELECTRONIQUE :</span>
+                            <ul>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                            </ul>
+                        </tr>
+                        <tr>
+                            <span class="titreMatieres"> PHYSIQUE-CHIMIE :</span>
+                            <ul>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                </li>
+                            </ul>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
