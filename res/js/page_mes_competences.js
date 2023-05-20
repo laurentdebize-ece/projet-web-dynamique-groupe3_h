7d03b4714a82bee439ebb3b800b7a574440ecd19
Jules FEDIT <jules.fedit@edu.ece.fr>
2023-05-20 18:10:11 +0200

    ajout des popups des filtres et ajouter

---
+++ res/js/page_mes_competences.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.filtres').click(function () {
+        $('#popup').show();
+        $('.fermer').click(function () {
+            $('#popup').hide();
+        });
+    })
+
+})
