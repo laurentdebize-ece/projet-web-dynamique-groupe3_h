4faa76f03297f8f5e39f2837f8b43af390debd9b
raphaël durand <raphael.durand@edu.ece.fr>
2023-05-23 00:22:28 +0200

    ajout bootstrap + header + jquery

--- page_ajouter_modifier_supprimer_competence_professeur.php
+++ page_ajouter_modifier_supprimer_competence_professeur.php
@@ -12,12 +12,20 @@ $sess->ensureHasAuthority(User::ACCOUNT_TYPE_USER);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="res/css/page_ajouter_modifier_supprimer_competence_professeur.css" rel="stylesheet" type="text/css" />
     <link rel="icon" type="icon_onglet.png" href="res/img/icon_onglet.png">
+    <link rel="stylesheet" href="res/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="res/css/toolbar.css" />
+    <script src="res/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <title>Evaluation</title>
 </head>
 
 <body>
+    <div class="toolbar">
+        <a href="/"><img class="logo" src="res/img/logo_skills_tracker_noir.png" alt="logo"></a>
+        <a href="/listing.php"><button class="toolbar-btn"> <strong>Mes matières</strong></button></a>
+        <a><button class="toolbar-btn"> <strong>Mes compétences</strong></button></a>
+    </div>
     <div class="maPopup" id="popup_ajouter">
         <div class="contenu_popup">
             <span class="fermer"><img src="res/img/x-circle.svg" alt="croix"></span>
@@ -62,8 +70,8 @@ $sess->ensureHasAuthority(User::ACCOUNT_TYPE_USER);
                             </select>
                         </td>
                     </tr>
-                        
-                    
+
+
                 </form>
             </table>
             <br>
@@ -72,25 +80,6 @@ $sess->ensureHasAuthority(User::ACCOUNT_TYPE_USER);
     </div>
     </div>
     <script type="text/javascript" src=" "></script>
-    <div id="header">
-        <table>
-            <tr>
-                <img class="logo" src="res/img/logo_skills_tracker_noir.png" alt="logo">
-            </tr>
-            <tr>
-                <button class="b1"> <strong>Mon compte</strong></button>
-            </tr>
-            <tr>
-                <a href="page_mes_competences.php"><button class="b1"><strong>Mes compétences</strong></button></a>
-            </tr>
-            <tr>
-                <button class="b1"> <strong>A propos</strong></button>
-            </tr>
-            <tr>
-                <button class="b1"> <strong>Contact</strong></button>
-            </tr>
-        </table>
-    </div>
     <div id="wrapper">
         <br>
         <h1> <strong>EVALUATION DES COMPETENCES</strong></h1>
