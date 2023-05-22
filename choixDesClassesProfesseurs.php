9f56707dc669e1a143abda505b1811317a91fd8a
raphaël durand <raphael.durand@edu.ece.fr>
2023-05-23 00:45:16 +0200

    ajout bootstrap + header +Jquery

--- choixDesClassesProfesseurs.php
+++ choixDesClassesProfesseurs.php
@@ -1,3 +1,9 @@
+<?php
+$sess = SessionManager::getInstance();
+$sess->ensureHasAuthority(User::ACCOUNT_TYPE_USER);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +11,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="res/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="res/css/toolbar.css" />
+<script src="res/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="res/css/choixDesClassesProfesseurs.css" type="text/css">
     <link rel="stylesheet" href="res/js/choixDesClassesProfesseurs.js" type="text/js">
     <link rel="icon" type="icon_onglet.png" href="icon_onglet.png">
@@ -12,24 +21,10 @@
 </head>
 
 <body class="background">
-    <div id="header">
-        <table>
-            <tr>
-                <img class="logo" src="res/img/logo_skills_tracker_noir.png" alt="logo">
-            </tr>
-            <tr>
-                <button class="b1"> <strong>Mon compte</strong></button>
-            </tr>
-            <tr>
-                <button class="b1"> <strong>Mes compétences</strong></button>
-            </tr>
-            <tr>
-                <button class="b1"> <strong>A propos</strong></button>
-            </tr>
-            <tr>
-                <button class="b1"> <strong>Contact</strong></button>
-            </tr>
-        </table>
+<div class="toolbar">
+        <a href="/"><img class="logo" src="res/img/logo_skills_tracker_noir.png" alt="logo"></a>
+        <a href="/listing.php"><button class="toolbar-btn"> <strong>Mes matières</strong></button></a>
+        <a><button class="toolbar-btn"> <strong>Mes compétences</strong></button></a>
     </div>
      <h1 id="texte2">
         <strong>
